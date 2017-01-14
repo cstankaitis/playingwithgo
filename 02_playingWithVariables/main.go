550153b064bcb18371b3f64467526e8e999ef5ab
Chris Stankaitis <stankaitis@pythian.com>
2017-01-14 10:44:32 -0500

    still playing with variables

--- 02_playingWithVariables/main.go
+++ 02_playingWithVariables/main.go
@@ -12,11 +12,11 @@ func main() {
 	f := `Do you like my hat?`
 	g := 'M'
 
-	fmt.Printf("%T \n", a)
-	fmt.Printf("%T \n", b)
-	fmt.Printf("%T \n", c)
-	fmt.Printf("%T \n", d)
-	fmt.Printf("%T \n", e)
-	fmt.Printf("%T \n", f)
-	fmt.Printf("%T \n", g)
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+	fmt.Printf("%v \n", g)
 }
