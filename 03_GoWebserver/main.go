05686074d77f107c00f1279751eda0fb492afb43
Chris Stankaitis <stankaitis@pythian.com>
2017-01-24 09:07:56 -0500

    Update

--- 03_GoWebserver/main.go
+++ 03_GoWebserver/main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello from a GOLang Webserver!")
+	io.WriteString(w, "Hello From A GOLang Webserver!")
 }
